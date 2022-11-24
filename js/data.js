b52eb0e082cb8e7f2e970691196f76a0ce7b4c64
Joaquin Huerta <joaco.011@gmail.com>
2022-11-24 15:30:07 -0300

    new features

---
+++ js/data.js
@@ -0,0 +1,26 @@
+
+const farmacos = {
+    ibuprofeno: {
+        presentacion: [
+            {name: 'Comprimidos 200mg', ammount: 200, units: 'mg' },
+            {name: 'Comprimidos 400mg', ammount: 400, units: 'mg' },
+            {name: 'Suspension Oral 100mg/5ml', ammount: 100, units: 'mg/5ml' },
+            {name: 'Suspension Oral 200mg/5ml', ammount: 200, units: 'mg/5ml' },
+        ],
+        dosis: {cant: 10, units: 'mg /Kg /dosis'} 
+    },
+    paracetamol: {
+        presentacion: [
+            {name: 'Gotas 100mg/ml (3xKg)', ammount: 100, units: 'mg/ml' },
+            {name: 'Comprimidos Masticables 80mg', ammount: 80, units: 'mg' },
+            {name: 'Comprimidos Masticables 160mg', ammount: 160, units: 'mg' },
+            {name: 'Suspension Oral 80mg/5ml', ammount: 80, units: 'mg/5ml' },
+            {name: 'Suspension Oral 120mg/5ml', ammount: 120, units: 'mg/5ml' },
+            {name: 'Suspension Oral 160mg/5ml', ammount: 160, units: 'mg/5ml' },
+            {name: 'Supositorio 125mg', ammount: 125, units: 'mg' },
+        ],
+        dosis: {cant: 15, units: 'mg /Kg /dosis'} 
+    },
+
+
+}
