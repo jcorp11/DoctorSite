78c75a125c2fc11034b9d913dddb84924e4ebd0b
Joaquin Huerta <joaco.011@gmail.com>
2022-11-28 17:27:01 -0300

    mas farmacos2

--- js/data.js
+++ js/data.js
@@ -129,5 +129,59 @@ const farmacos = {
         ],
         dosis: {ammount: 15, units: 'mg /Kg /dia'} 
     },
+    ceftriaxona: {
+        presentacion: [
+            {name: 'Polvo para inyeccion: 250mg', ammount: 250, units: 'mg' },
+            {name: 'Polvo para inyeccion: 1g', ammount: 1000, units: 'mg' },
+        ],
+        dosis: {ammount: 60/2, units: 'mg /Kg /dosis /dia'} 
+    },
+    ampicilina: {
+        presentacion: [
+            {name: 'Comprimidos: 500mg', ammount: 500, units: 'mg' },
+            {name: 'Suspension Oral: 250mg/5ml', ammount: 50, units: 'mg /ml' },
+        ],
+        dosis: {ammount: (50+25)/2, units: 'mg /Kg /dosis'} 
+    },
+    domperidona: {
+        presentacion: [
+            {name: 'Comprimidos: 10mg', ammount: 10, units: 'mg' },
+            {name: 'Gotas: 10mg/ml (1xKg)', ammount: 10, units: 'mg /ml' },
+        ],
+        dosis: {ammount: '', units: 'mg /Kg /dosis'} 
+    },
+    pargeverina: {
+        presentacion: [
+            {name: 'Gotas: 5mg/ml', ammount: 10, units: 'mg /ml' },
+        ],
+        dosis: {ammount: '', units: 'mg /Kg /dosis'} 
+    },
+    cloxacilina: {
+        presentacion: [
+            {name: 'Comprimidos: 500mg', ammount: 500, units: 'mg' },
+            {name: 'Suspension Oral: 125mg/5ml', ammount: 125/5, units: 'mg /ml' },
+            {name: 'Suspension Oral: 250mg/5ml', ammount: 250/5, units: 'mg /ml' },
+        ],
+        dosis: {ammount: 50, units: 'mg /Kg /dia'} 
+    },
+    clindamicina: {
+        presentacion: [
+            {name: 'Suspension Oral: 600mg/4ml', ammount: 600/4, units: 'mg /ml' },
+        ],
+        dosis: {ammount: (5+7.5)/2, units: 'mg /Kg /dia'} 
+    },
+    levocetericina: {
+        presentacion: [
+            {name: 'Suspension Oral: 25mg/5ml', ammount: 25/5, units: 'mg /ml' },
+        ],
+        dosis: {ammount: '', units: 'mg /Kg /dia'} 
+    },
+    clorfenamina: {
+        presentacion: [
+            {name: 'Suspension Oral: 2mg/5ml', ammount: 2/5, units: 'mg /ml' },
+            {name: 'Inyectable: 4mg/ml', ammount: 4, units: 'mg /ml' },
+        ],
+        dosis: {ammount: 0.1, units: 'mg /Kg /dosis'} 
+    },
 }
 
